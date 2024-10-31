6a8e913bc5779b5e3a92baa3de607bd072f231e0
salt1 <70152208+salt1@users.noreply.github.com>
2024-11-01 02:35:04 +0900

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -8,7 +8,9 @@ def home():
     return "Hello, I am alive!"
 
 def run():
-    port = int(os.environ.get("PORT", 10000))  # Render는 포트 10000을 사용
+    if __name__ == "__main__":
+    # Render에서 자동으로 설정한 PORT 환경 변수를 사용하여 서버 실행
+    port = int(os.environ.get("PORT", 8080))  # PORT 환경 변수가 없을 경우 8080 사용
     app.run(host='0.0.0.0', port=port)
     
 def keep_alive():
