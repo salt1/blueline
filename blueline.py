d1981932f6e009678803c74d1b71e38dad1452bf
salt1 <70152208+salt1@users.noreply.github.com>
2024-11-01 02:17:29 +0900

    Update blueline.py

--- blueline.py
+++ blueline.py
@@ -1,26 +1,22 @@
 import discord
 from discord.ext import commands
 import os
-import webserver
 
 DISCORD_TOKEN = os.environ['discordkey']
+MENTIONED_USER_ID = '714355124146929684'
 
-class Client(discord.Client):
-
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
-
-        if message.content.startswith('---'):
-            await message.channel.send(f'```ansi\n[2;34m---------------------------------------------------------------------------------------------------------------[0m\n```')
-
-# 인텐트 설정
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-webserver.keep_alive()
+@bot.event
+async def on_message(message):
+    if MENTIONED_USER_ID in [str(user.id) for user in message.mentions]:
+        try:
+            for i in range(50):
+                await message.author.send("GET SPAMMED")
+        except discord.Forbidden:
+            await message.channel.send(f"{message.author.mention}, I tried to DM you but couldn't. Please enable DMs.")
+    await bot.process_commands(message)
+
 bot.run(DISCORD_TOKEN)
